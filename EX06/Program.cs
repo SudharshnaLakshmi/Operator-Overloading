8276d3739ac8ae4253d5c37a00db77f1dfbbb635
Sudharshna Lakshmi.S <sudharshnalakshmi@gmail.com>
2023-05-08 14:51:39 +0530

    EX06

--- EX06/Program.cs
+++ EX06/Program.cs
@@ -1,23 +1,4 @@
-# Operator-Overloading
-
-## Aim:
- To write a C# program to pass values through constructors(default and parameterized) and also overload equal operators by checking whether objects are equal using operator overloading. 
- 
-## Algorithm:
-### Step 1:
-Create a class operator
-### Step 2:
-Pass values through the constructor
-### Step 3:
-return the bool operator, (==) and (!=)
-### Step 4:
-create a object to store the return object
-### Step 5:
-print the program.
- 
-## Program:
-```
-using System;
+﻿using System;
 namespace EX06
 {
     class Program
@@ -57,9 +38,7 @@ namespace EX06
         }
     }
 }
-```
-## Output:
-![](1.png)
 
-## Result:
-Thus a C# program to pass values through constructors(default and parameterized) and also overload equal operators by checking whether objects are equal using operator overloading executed successfully
+
+
+
